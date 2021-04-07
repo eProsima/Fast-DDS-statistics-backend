0db490a656fbb72fb4a446f525c46e86e3d3bdf5
Eduardo Ponz Segrelles <eduardoponz@eprosima.com>
2021-04-07 08:52:01 +0200

    Enable constructing an EntityId from an int (#31)
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>

--- test/unittest/EntityId/EntityIdTests.cpp
+++ test/unittest/EntityId/EntityIdTests.cpp
@@ -21,18 +21,6 @@
 
 using namespace eprosima::statistics_backend;
 
-class EntityIdTest : public EntityId
-{
-public:
-
-    EntityIdTest(
-            int64_t value)
-        : EntityId(value)
-    {
-    }
-
-};
-
 TEST(entityid, all)
 {
     EXPECT_EQ(0, EntityId::all().value());
@@ -45,7 +33,7 @@ TEST(entityid, invalid)
 
 TEST(entityid, invalidate)
 {
-    EntityIdTest entity_id(1);
+    EntityId entity_id(1);
     EXPECT_TRUE(entity_id.is_valid());
     entity_id.invalidate();
     EXPECT_FALSE(entity_id.is_valid());
@@ -55,21 +43,21 @@ TEST(entityid, valid)
 {
     EXPECT_FALSE(EntityId().is_valid());
     EXPECT_FALSE(EntityId::invalid().is_valid());
-    EXPECT_TRUE(EntityIdTest(0).is_valid());
-    EXPECT_TRUE(EntityIdTest(1).is_valid());
-    EXPECT_FALSE(EntityIdTest(-5).is_valid());
+    EXPECT_TRUE(EntityId(0).is_valid());
+    EXPECT_TRUE(EntityId(1).is_valid());
+    EXPECT_FALSE(EntityId(-5).is_valid());
 }
 
 TEST(entityid, value)
 {
     int64_t value = 12;
-    EXPECT_EQ(value, EntityIdTest(value).value());
+    EXPECT_EQ(value, EntityId(value).value());
 }
 
 TEST(entityid, operator_ostream)
 {
     int64_t value = 12;
-    EntityIdTest entity_id(value);
+    EntityId entity_id(value);
     std::stringstream out;
     out << entity_id;
     std::stringstream check;
@@ -79,17 +67,17 @@ TEST(entityid, operator_ostream)
 
 TEST(entityid, operator_less)
 {
-    EntityIdTest entity_id_1(1);
-    EntityIdTest entity_id_2(2);
+    EntityId entity_id_1(1);
+    EntityId entity_id_2(2);
     EXPECT_TRUE(entity_id_1 < entity_id_2);
     EXPECT_FALSE(entity_id_2 < entity_id_1);
 }
 
 TEST(entityid, operator_less_equal)
 {
-    EntityIdTest entity_id_1(1);
-    EntityIdTest entity_id_2(2);
-    EntityIdTest entity_id_3(2);
+    EntityId entity_id_1(1);
+    EntityId entity_id_2(2);
+    EntityId entity_id_3(2);
     EXPECT_TRUE(entity_id_1 <= entity_id_2);
     EXPECT_TRUE(entity_id_2 <= entity_id_3);
     EXPECT_FALSE(entity_id_2 <= entity_id_1);
@@ -97,17 +85,17 @@ TEST(entityid, operator_less_equal)
 
 TEST(entityid, operator_greater)
 {
-    EntityIdTest entity_id_1(1);
-    EntityIdTest entity_id_2(2);
+    EntityId entity_id_1(1);
+    EntityId entity_id_2(2);
     EXPECT_TRUE(entity_id_2 > entity_id_1);
     EXPECT_FALSE(entity_id_1 > entity_id_2);
 }
 
 TEST(entityid, operator_greater_equal)
 {
-    EntityIdTest entity_id_1(1);
-    EntityIdTest entity_id_2(2);
-    EntityIdTest entity_id_3(2);
+    EntityId entity_id_1(1);
+    EntityId entity_id_2(2);
+    EntityId entity_id_3(2);
     EXPECT_TRUE(entity_id_2 >= entity_id_1);
     EXPECT_TRUE(entity_id_3 >= entity_id_2);
     EXPECT_FALSE(entity_id_1 >= entity_id_2);
@@ -115,18 +103,18 @@ TEST(entityid, operator_greater_equal)
 
 TEST(entityid, operator_equal)
 {
-    EntityIdTest entity_id_1(1);
-    EntityIdTest entity_id_2(2);
-    EntityIdTest entity_id_3(2);
+    EntityId entity_id_1(1);
+    EntityId entity_id_2(2);
+    EntityId entity_id_3(2);
     EXPECT_FALSE(entity_id_1 == entity_id_2);
     EXPECT_TRUE(entity_id_2 == entity_id_3);
 }
 
 TEST(entityid, operator_different)
 {
-    EntityIdTest entity_id_1(1);
-    EntityIdTest entity_id_2(2);
-    EntityIdTest entity_id_3(2);
+    EntityId entity_id_1(1);
+    EntityId entity_id_2(2);
+    EntityId entity_id_3(2);
     EXPECT_FALSE(entity_id_2 != entity_id_3);
     EXPECT_TRUE(entity_id_1 != entity_id_2);
 }
