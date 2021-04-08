f2ff73bbd27b29af8129d3567516597c3bdfaf2c
jparisu <69341543+jparisu@users.noreply.github.com>
2021-04-08 10:00:06 +0200

    Change include name (#32)
    * Refs #11109: change include name of json
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #11109: update .repos file
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #11109: apply changes
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/cpp/database/data.hpp
+++ src/cpp/database/data.hpp
@@ -22,8 +22,8 @@
 
 #include "samples.hpp"
 
+#include <fastdds-statistics-backend/nlohmann-json/json.hpp>
 #include <fastdds-statistics-backend/types/EntityId.hpp>
-#include <nlohmann-json/json.hpp>
 
 #include <chrono>
 #include <vector>
