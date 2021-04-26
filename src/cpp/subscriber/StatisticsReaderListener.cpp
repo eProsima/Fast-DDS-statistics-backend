98ee60e3ac194b962d7cd83b0f363e53260a33e7
Iker Luengo <ikerluengo@eprosima.com>
2021-04-26 17:37:47 +0200

    Refs 11093. Complete Data listener implementation
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- src/cpp/subscriber/StatisticsReaderListener.cpp
+++ src/cpp/subscriber/StatisticsReaderListener.cpp
@@ -78,6 +78,101 @@ void StatisticsReaderListener::on_data_available(
                 process = true;
             }
             break;
+        case EventKind::NETWORK_LATENCY:
+            if (data_mask_.is_set(DataKind::NETWORK_LATENCY))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::PUBLICATION_THROUGHPUT:
+            if (data_mask_.is_set(DataKind::PUBLICATION_THROUGHPUT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::SUBSCRIPTION_THROUGHPUT:
+            if (data_mask_.is_set(DataKind::SUBSCRIPTION_THROUGHPUT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::RTPS_SENT:
+            if (data_mask_.is_set(DataKind::RTPS_PACKETS_SENT)  ||
+                data_mask_.is_set(DataKind::RTPS_BYTES_SENT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::RTPS_LOST:
+            if (data_mask_.is_set(DataKind::RTPS_PACKETS_LOST)  ||
+                data_mask_.is_set(DataKind::RTPS_BYTES_LOST))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::RESENT_DATAS:
+            if (data_mask_.is_set(DataKind::RESENT_DATA))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::HEARTBEAT_COUNT:
+            if (data_mask_.is_set(DataKind::HEARTBEAT_COUNT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::ACKNACK_COUNT:
+            if (data_mask_.is_set(DataKind::ACKNACK_COUNT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::NACKFRAG_COUNT:
+            if (data_mask_.is_set(DataKind::NACKFRAG_COUNT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::GAP_COUNT:
+            if (data_mask_.is_set(DataKind::GAP_COUNT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::DATA_COUNT:
+            if (data_mask_.is_set(DataKind::DATA_COUNT))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::PDP_PACKETS:
+            if (data_mask_.is_set(DataKind::PDP_PACKETS))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::EDP_PACKETS:
+            if (data_mask_.is_set(DataKind::EDP_PACKETS))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::DISCOVERED_ENTITY:
+            if (data_mask_.is_set(DataKind::DISCOVERY_TIME))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::SAMPLE_DATAS:
+            if (data_mask_.is_set(DataKind::SAMPLE_DATAS))
+            {
+                process = true;
+            }
+            break;
+        case EventKind::PHYSICAL_DATA:
+            process = true;
+            break;
     }
 
     if (process)
