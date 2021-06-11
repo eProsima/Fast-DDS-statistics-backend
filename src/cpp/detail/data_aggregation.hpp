e041094ce24c580add2d7c7b6eab45d30f98ce85
Miguel Company <miguelcompany@eProsima.com>
2021-06-11 09:51:30 +0200

    Refs 11475. Added doxydoc to aggregators.
    Signed-off-by: Miguel Company <miguelcompany@eProsima.com>

--- src/cpp/detail/data_aggregation.hpp
+++ src/cpp/detail/data_aggregation.hpp
@@ -34,10 +34,28 @@ namespace eprosima {
 namespace statistics_backend {
 namespace detail {
 
+/**
+ * Generic interface for aggregating raw statistics data into several bins.
+ *
+ * It is constructed with the bins configuration and a reference to the resulting
+ * data collection.
+ *
+ * The data returned by the database select() method is processed by the @ref add_data
+ * method.
+ *
+ * To perform final aggregation calculations, method @ref finish should be called.
+ */
 struct IDataAggregator
 {
     virtual ~IDataAggregator() = default;
 
+    /**
+     * @brief Construct an IDataAggregator.
+     * @param bins           Number of time intervals in which the measurement time is divided
+     * @param t_from         Starting time of the returned measures
+     * @param t_to           Ending time of the returned measures
+     * @param returned_data  Reference to the collection to be returned by @ref StatisticsBackend::get_data
+     */
     IDataAggregator(
             uint16_t bins,
             Timestamp t_from,
@@ -49,33 +67,65 @@ struct IDataAggregator
         prepare_bins(bins, t_from, t_to);
     }
 
+    /**
+     * @brief Process a collection of data returned by the database.
+     * @param iterators Reference to the iterator pair returned by @ref get_iterators
+     */
     void add_data(
             IteratorPair& iterators)
     {
         for (auto& it = *iterators.first; it != *iterators.second; ++it)
         {
+            // Find and check the bin corresponding to the sample timestamp
             Timestamp ts = it.get_timestamp();
             auto index = (ts - data_[0].first) / interval_;
             assert((index >= 0) && static_cast<size_t>(index) < data_.size());
+
+            // Add sample value to the corresponding bin
             add_sample(static_cast<size_t>(index), it.get_value());
         }
     }
 
+    /**
+     * @brief Performs final aggregation of data.
+     */
     virtual void finish()
     {
     }
 
 protected:
 
+    /**
+     * @brief Add a sample to a specific bin.
+     * @param index Index of the bin where the sample should be added.
+     * @param value Statistical value to add.
+     */
     virtual void add_sample(
             size_t index,
             double value) = 0;
 
+    /**
+     * @brief Get the initial aggregation value.
+     * @return Initial aggregation value.
+     */
     virtual double initial_value() const noexcept
     {
+        // All aggregators except @ref CountAggregator will be initialized to quiet_NaN
         return std::numeric_limits<double>::quiet_NaN();
     }
 
+    /**
+     * @brief Assign a value to a bin if it was not given a previous value.
+     *
+     * This method is used on aggregators that need to take special actions the first time
+     * a value is added to a bin.
+     * For instance, the @ref MaximumAggregator will assign the value the first time, and
+     * will perform a comparison with the current value on the following ones.
+     *
+     * @param index Index of the bin where the sample should be added.
+     * @param value Statistical value to assign.
+     * @return whether the value has been assigned to the bin.
+     */
     bool assign_if_nan(
             size_t index,
             double value)
@@ -89,6 +139,7 @@ protected:
         return false;
     }
 
+    //! Reference to the collection to be returned by @ref StatisticsBackend::get_data
     std::vector<StatisticsData>& data_;
 
 private:
@@ -98,10 +149,14 @@ private:
             Timestamp t_from,
             Timestamp t_to)
     {
+        // Perform a single allocation for all the returned bins
         data_.reserve(bins);
+
+        // Calculate the duration of each bin
         interval_ = t_to - t_from;
         interval_ /= bins;
 
+        // Fill each bin with the initial value and the initial timestamp of the bin
         double value = initial_value();
         do
         {
@@ -111,9 +166,11 @@ private:
         } while (bins > 0);
     }
 
+    //! Duration of each bin
     Timestamp::duration interval_;
 };
 
+/// An @ref IDataAggregator that returns the first sample received for each bin
 struct NoneAggregator final : public IDataAggregator
 {
     NoneAggregator(
@@ -136,6 +193,7 @@ protected:
 
 };
 
+/// An @ref IDataAggregator that returns the addition of all the samples added to each bin
 struct SumAggregator final : public IDataAggregator
 {
     SumAggregator(
@@ -161,6 +219,12 @@ protected:
 
 };
 
+/**
+ * @brief An @ref IDataAggregator that returns the mean value of all the samples added to each bin
+ *
+ * It behaves as the @ref SumAggregator, but it also keeps track of the number of samples added to each bin.
+ * It will then perform the final division inside @ref MeanAggregator::finish.
+ */
 struct MeanAggregator final : public IDataAggregator
 {
     MeanAggregator(
@@ -203,10 +267,17 @@ protected:
 
 private:
 
+    /// Keeps the number of samples added to each bin
     std::vector<size_t> num_samples_;
 
 };
 
+/**
+ * @brief An @ref IDataAggregator that returns the median value of all the samples added to each bin
+ *
+ * It keeps the collection of all the samples received on each bin, and finds the median on
+ * @ref MedianAggregator::finish.
+ */
 struct MedianAggregator final : public IDataAggregator
 {
     MedianAggregator(
@@ -226,17 +297,27 @@ struct MedianAggregator final : public IDataAggregator
             size_t n_samples = samples_[n].size();
             if (n_samples > 0)
             {
+                // For an odd number of samples, this will give us the index of the middle sample.
+                // For an even number of samples, this will give us the first index of the two middle samples.
                 size_t index = (n_samples - 1) / 2;
+
+                // We use nth_element to perform a partial ordering of the collection
                 auto begin = samples_[n].begin();
                 auto end = samples_[n].end();
                 auto med = begin;
                 std::advance(med, index);
                 std::nth_element(begin, med, end);
+
+                // The sample at index is now the median.
+                // All samples before index are lower. All samples after index are greater.
                 double median = *med;
                 if (0 == (n_samples % 2))
                 {
+                    // Perform partial ordering of the items after index so the first one is the lowest of them.
                     ++med;
                     std::nth_element(med, med, end);
+
+                    // Median is the mean of the two center values
                     median = (median + *med) / 2;
                 }
 
@@ -256,10 +337,12 @@ protected:
 
 private:
 
+    /// Keeps all the values added to each bin
     std::vector<std::vector<double>> samples_;
 
 };
 
+/// An @ref IDataAggregator that returns the maximum of all the samples added to each bin
 struct MaximumAggregator final : public IDataAggregator
 {
     MaximumAggregator(
@@ -285,6 +368,7 @@ protected:
 
 };
 
+/// An @ref IDataAggregator that returns the minimum of all the samples added to each bin
 struct MinimumAggregator final : public IDataAggregator
 {
     MinimumAggregator(
@@ -310,6 +394,7 @@ protected:
 
 };
 
+/// An @ref IDataAggregator that returns the number of samples added to each bin
 struct CountAggregator final : public IDataAggregator
 {
     CountAggregator(
@@ -338,8 +423,12 @@ protected:
 
 };
 
-// Implements the naïve algorithm for standard deviation calculation
-// (https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Na%C3%AFve_algorithm)
+/**
+ * @brief An @ref IDataAggregator that returns the maximum of all the samples added to each bin.
+ *
+ * Implements the <a href="https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Na%C3%AFve_algorithm">naïve algorithm</a>
+ * keeping the sum of the values, the sum of the squares, and the number of samples.
+ */
 struct StdDevAggregator final : public IDataAggregator
 {
     StdDevAggregator(
@@ -380,6 +469,7 @@ protected:
 
 private:
 
+    /// Holds the calculation data required for one bin
     struct BinData
     {
         double sum = 0;
@@ -387,12 +477,22 @@ private:
         uint64_t num_samples = 0;
     };
 
+    /// Holds the calculation data required for each bin
     std::vector<BinData> bin_data_;
 
 };
 
 } // namespace detail
 
+/**
+ * @brief Get the aggregator required for processing a @ref StatisticsBackend::get_data call.
+ * @param bins           Number of time intervals in which the measurement time is divided
+ * @param t_from         Starting time of the returned measures
+ * @param t_to           Ending time of the returned measures
+ * @param statistic      Kind of aggregation to perform
+ * @param returned_data  Reference to the collection to be returned by @ref StatisticsBackend::get_data
+ *
+ */
 std::unique_ptr<detail::IDataAggregator> get_data_aggregator(
         uint16_t bins,
         Timestamp t_from,
