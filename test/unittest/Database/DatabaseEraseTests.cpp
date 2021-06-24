6696642b768f328c18fe5ad2681b19cb7902aca4
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2021-06-24 14:55:33 +0200

    Refs #11090: add negative tests
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- test/unittest/Database/DatabaseEraseTests.cpp
+++ test/unittest/Database/DatabaseEraseTests.cpp
@@ -24,6 +24,7 @@ constexpr const char* FINAL_DUMP_FILE = "resources/complex_dump_erased_domain_1.
 #include <gtest_aux.hpp>
 #include <gtest/gtest.h>
 
+#include <exception/Exception.hpp>
 #include <types/JSONTags.h>
 #include <types/types.hpp>
 
@@ -76,6 +77,9 @@ TEST(database_erase_tests, erase_domain)
     std::vector<std::pair<EntityId, EntityId>> domains = db.get_entities_by_name(EntityKind::DOMAIN, "domain_1");
     db.erase(domains.begin()->first);
 
+    // Calling again to erase an already erased domain throws an exception
+    ASSERT_THROW(db.erase(domains.begin()->first), BadParameter);
+
     // Dump erased database
     DatabaseDump erased_dump = db.dump_database();
 
@@ -91,6 +95,51 @@ TEST(database_erase_tests, erase_domain)
     EXPECT_EQ(final_dump[DATAREADER_CONTAINER_TAG], erased_dump[DATAREADER_CONTAINER_TAG]);
 
     EXPECT_EQ(final_dump, erased_dump);
+
+}
+
+// This test checks that calling erase with an EntityId that does not correspond with EntityKind::DOMAIN, kills the
+// thread with an assertion
+TEST(database_erase_tests, erase_wrong_kind)
+{
+#ifndef NDEBUG
+    // Read JSON files
+    DatabaseDump initial_dump = load_file(INITIAL_DUMP_FILE);
+
+    // Create database
+    Database db;
+
+    // Load initial dump in the database
+    db.load_database(initial_dump);
+
+    // Call erase monitor with EntityKind::HOST
+    std::vector<std::pair<EntityId, EntityId>> hosts = db.get_entities_by_name(EntityKind::HOST, "host_0");
+    ASSERT_DEATH(db.erase(hosts.begin()->second), "");
+    // Call erase monitor with EntityKind::USER
+    std::vector<std::pair<EntityId, EntityId>> users = db.get_entities_by_name(EntityKind::USER, "user_0");
+    ASSERT_DEATH(db.erase(users.begin()->second), "");
+    // Call erase monitor with EntityKind::PROCESS
+    std::vector<std::pair<EntityId, EntityId>> processes = db.get_entities_by_name(EntityKind::PROCESS, "process_0");
+    ASSERT_DEATH(db.erase(processes.begin()->second), "");
+    // Call erase monitor with EntityKind::TOPIC
+    std::vector<std::pair<EntityId, EntityId>> topics = db.get_entities_by_name(EntityKind::TOPIC, "topic_0");
+    ASSERT_DEATH(db.erase(topics.begin()->second), "");
+    // Call erase monitor with EntityKind::PARTICIPANT
+    std::vector<std::pair<EntityId, EntityId>> participants = db.get_entities_by_name(EntityKind::PARTICIPANT,
+        "participant_0");
+    ASSERT_DEATH(db.erase(participants.begin()->second), "");
+    // Call erase monitor with EntityKind::DATAWRITER
+    std::vector<std::pair<EntityId, EntityId>> writers = db.get_entities_by_name(EntityKind::DATAWRITER,
+        "datawriter_0");
+    ASSERT_DEATH(db.erase(writers.begin()->second), "");
+    // Call erase monitor with EntityKind::DATAREADER
+    std::vector<std::pair<EntityId, EntityId>> readers = db.get_entities_by_name(EntityKind::DATAREADER,
+        "datareader_0");
+    ASSERT_DEATH(db.erase(readers.begin()->second), "");
+    // Call erase monitor with EntityKind::LOCATOR
+    std::vector<std::pair<EntityId, EntityId>> locators = db.get_entities_by_name(EntityKind::LOCATOR, "locator_0");
+    ASSERT_DEATH(db.erase(locators.begin()->second), "");
+#endif // NDEBUG
 }
 
 int main(
