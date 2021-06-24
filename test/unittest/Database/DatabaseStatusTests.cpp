e26038e110320decb936583df063f21df4b28ab2
serranoePros <raulserrrano@eprosima.com>
2021-06-24 16:24:42 +0200

    Refs #11667: Windows fix?
    Signed-off-by: serranoePros <raulserrrano@eprosima.com>

--- test/unittest/Database/DatabaseStatusTests.cpp
+++ test/unittest/Database/DatabaseStatusTests.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <gtest_aux.hpp>
 #include <gtest/gtest.h>
 
 #include <database/database.hpp>
