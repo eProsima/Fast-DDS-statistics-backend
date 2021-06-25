80db8893e2fa722f07cc856b36c68709f9494919
Iker Luengo <ikerluengo@eprosima.com>
2021-06-25 08:42:46 +0200

    Refs 11864. uncrustify
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- test/unittest/Database/DatabaseLoadInsertTests.cpp
+++ test/unittest/Database/DatabaseLoadInsertTests.cpp
@@ -155,11 +155,11 @@ public:
             Map const& rhs)
     {
         return lhs.size() == rhs.size()
-            && std::equal(lhs.begin(), lhs.end(), rhs.begin(),
-                    [](auto a, auto b)
-                    {
-                        return a.first == b.first;
-                    });
+               && std::equal(lhs.begin(), lhs.end(), rhs.begin(),
+                       [](auto a, auto b)
+                       {
+                           return a.first == b.first;
+                       });
     }
 
     template <typename Map>
@@ -169,8 +169,8 @@ public:
     {
         // No predicate needed because there is operator== for pairs already.
         return lhs.size() == rhs.size()
-            && std::equal(lhs.begin(), lhs.end(),
-                    rhs.begin());
+               && std::equal(lhs.begin(), lhs.end(),
+                       rhs.begin());
     }
 
     DataBaseTest db;
