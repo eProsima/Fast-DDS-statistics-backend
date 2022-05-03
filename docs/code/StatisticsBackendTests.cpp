c1a5c5bc584e52d3affcfab5b62670238d9a63c9
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2022-05-03 12:48:51 +0200

    Refs #14468, #14669: DNS names could also be used
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- docs/code/StatisticsBackendTests.cpp
+++ docs/code/StatisticsBackendTests.cpp
@@ -54,7 +54,7 @@ void init_monitor_examples()
         // "44.53.01.5f.45.50.52.4f.53.49.4d.41".
         // The monitor has no listener associated.
         EntityId disc_server_prefix_monitor_id =
-                StatisticsBackend::init_monitor("44.53.01.5f.45.50.52.4f.53.49.4d.41", "UDPv4:[127.0.0.1]:11811");
+                StatisticsBackend::init_monitor("44.53.01.5f.45.50.52.4f.53.49.4d.41", "UDPv4:[localhost]:11811");
         //!--
         static_cast<void>(domain_monitor_id);
         static_cast<void>(disc_server_monitor_id);
@@ -106,7 +106,7 @@ void init_monitor_examples()
         // eprosima::fastdds::rtps::DEFAULT_ROS2_SERVER_GUIDPREFIX.
         // The monitor uses a custom listener, a CallbackMask, and a DataKindMask.
         EntityId disc_server_monitor_id =
-                StatisticsBackend::init_monitor("UDPv4:[127.0.0.1]:11811", &domain_listener, callback_mask, datakind_mask);
+                StatisticsBackend::init_monitor("UDPv4:[localhost]:11811", &domain_listener, callback_mask, datakind_mask);
 
         // Init a monitor for a Fast DDS Discovery Server network which server is located in IPv4
         // address 127.0.0.1 and port 11811 using UDP transport layer, and that uses the GUID prefix
