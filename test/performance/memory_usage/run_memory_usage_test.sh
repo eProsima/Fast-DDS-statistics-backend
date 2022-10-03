34c4112e53e37af50d653e43eebca58c1e6f4b2f
jparisu <javierparis@eprosima.com>
2022-10-03 14:22:26 +0200

    Final implementation with RSS
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- test/performance/memory_usage/run_memory_usage_test.sh
+++ test/performance/memory_usage/run_memory_usage_test.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
+# How to launch from a colcon workspace:
+# bash ./src/fastdds_statistics_backend/test/performance/memory_usage/run_memory_usage_test.sh --fastdds ./build/fastrtps/examples/cpp/dds/BasicConfigurationExample/BasicConfigurationExample --backend ./build/fastdds_statistics_backend/examples/cpp/HelloWorldExample/HelloWorldExample
+
 ############################################################
 # Script Arguments
 ############################################################
 
 WORKSPACE_DIR=$(pwd)
 FASTDDS_EXAMPLE_EXECUTABLE="${WORKSPACE_DIR}/BasicConfigurationExample/BasicConfigurationExample"
-BACKEND_EXAMPLE_EXECUTABLE="${WORKSPACE_DIR}/HelloWorldExampleHelloWorldExample"
+BACKEND_EXAMPLE_EXECUTABLE="${WORKSPACE_DIR}/HelloWorldExample/HelloWorldExample"
 
 RESULT_PAHT='./result'
-RESULT_FILE="memory_usage.csv"
-MEASURAMENT_RATE=0.2
+RESULT_FILE="backend_memory_usage.csv"
 LIBRARIES_TO_MEASURE="libfastcdr libfastrtps libfastdds_statistics_backend stack heap anon"
 
-ENTITIES_LOOP_ITERATIONS=2
-ENTITIES_LOOP_ELAPSED=10
+ENTITIES_LOOP_ITERATIONS=5
+ENTITIES_LOOP_ELAPSED=30
 ENTITIES_IN_LOOP=3
 STATISTIC_TOPICS="HISTORY_LATENCY_TOPIC;NETWORK_LATENCY_TOPIC;PUBLICATION_THROUGHPUT_TOPIC;SUBSCRIPTION_THROUGHPUT_TOPIC;RTPS_SENT_TOPIC;RTPS_LOST_TOPIC;HEARTBEAT_COUNT_TOPIC;ACKNACK_COUNT_TOPIC;NACKFRAG_COUNT_TOPIC;GAP_COUNT_TOPIC;DATA_COUNT_TOPIC;RESENT_DATAS_TOPIC;SAMPLE_DATAS_TOPIC;PDP_PACKETS_TOPIC;EDP_PACKETS_TOPIC;DISCOVERY_TOPIC;PHYSICAL_DATA_TOPIC"
 SLEEP_RESIDUAL_TIME=2
 
 PUBLICATION_RATE=10
-NUMBER_OF_TOPICS=2
+RESET_BACKEND=" "
+BUMP_BACKEND=" "
 
+DEBUG=0
 
 ############################################################
 # Functions needed in script
 ############################################################
+
+# pmap result:
+# column 1: Initial memory address
+# column 2: Virtual memory
+# column 3: RSS
 
 # TODO comment
 get_memory_usage_from_library () {
@@ -47,13 +56,16 @@ get_memory_usage_from_library () {
 
             LINE_=${line}
 
-            NEW_VALUE_WITH_K_="$(echo -n "${LINE_}" | awk '{print $2}')"
+            # Column 1 ->
+            NEW_VALUE_WITH_K_="$(echo -n "${LINE_}" | awk '{print $3}')"
 
-            NEW_VALUE_="$(echo -n "${NEW_VALUE_WITH_K_::-1}")"
+            # This is only needed without --extended to remove K
+            # NEW_VALUE_="$(echo -n "${NEW_VALUE_WITH_K_::-1}")"
+            NEW_VALUE_=${NEW_VALUE_WITH_K_}
 
             RESULT_=$((${RESULT_}+${NEW_VALUE_}))
 
-        done <<< "$(pmap ${PID_} | grep ${LIBRARY_NAME_})"
+        done <<< "$(pmap ${PID_} --extended | grep ${LIBRARY_NAME_})"
 
     fi
 
@@ -83,13 +95,16 @@ print_memory_usage () {
     done
 
     # Print total
-    TOTAL_MEMORY_USAGE_="$(pmap ${PID_} | tail -n 1 | awk '/[0-9]/K{print $2}')"
+    # TOTAL_MEMORY_USAGE_="$(pmap ${PID_} | tail -n 1 | awk '/[0-9]/K{print $2}')"
+    TOTAL_MEMORY_USAGE_="$(pmap ${PID_} --extended | tail -n 1 | awk '{print $4}')"
     # If there are lines, analyze them
     if [ ! -z "$PMAP_RESULT_" ];
     then
-        echo -n "${TOTAL_MEMORY_USAGE_::-1}" >> ${RESULT_FILE_}
+        # This is only needed without --extended to remove K
+        # echo -n "${TOTAL_MEMORY_USAGE_::-1}" >> ${RESULT_FILE_}
+        echo -n "${TOTAL_MEMORY_USAGE_}" >> ${RESULT_FILE_}
     else
-        echo -n "-" >> ${RESULT_FILE_}
+        echo -n "0" >> ${RESULT_FILE_}
     fi
 
     echo "" >> ${RESULT_FILE_}
@@ -132,11 +147,6 @@ do
         shift # past argument
         shift # past value
         ;;
-        --measurament-rate)
-        MEASURAMENT_RATE="$2"
-        shift # past argument
-        shift # past value
-        ;;
         --measurament-libraries)
         LIBRARIES_TO_MEASURE="$2"
         shift # past argument
@@ -149,7 +159,7 @@ do
         shift # past value
         ;;
         --loop-elapsed)
-        ENTITIES_LOOP_ELAPSE="$2"
+        ENTITIES_LOOP_ELAPSED="$2"
         shift # past argument
         shift # past value
         ;;
@@ -163,21 +173,24 @@ do
         shift # past argument
         shift # past value
         ;;
-        --residual-time)
-        SLEEP_RESIDUAL_TIME="$2"
-        shift # past argument
-        shift # past value
-        ;;
 
         --publication-rate)
         PUBLICATION_RATE="$2"
         shift # past argument
         shift # past value
         ;;
-        --n-topics)
-        NUMBER_OF_TOPICS="$2"
+        --reset)
+        RESET_BACKEND=" --reset "
         shift # past argument
-        shift # past value
+        ;;
+        --bump)
+        BUMP_BACKEND=" --bump-file __tmp_backend_test_bump_ "
+        shift # past argument
+        ;;
+
+        --debug)
+        DEBUG=1
+        shift # past argument
         ;;
 
         *)    # unknown option
@@ -230,13 +243,20 @@ echo "total(KB)" >> ${RESULT_FILE_PATH}
 ############################################################
 echo
 echo "---------------------------------------------------------------------------"
-echo "Executing Fast DDS Statistics Backend exaple ${BACKEND_EXAMPLE_EXECUTABLE}"
+echo "Executing Fast DDS Statistics Backend example ${BACKEND_EXAMPLE_EXECUTABLE}"
 echo "---------------------------------------------------------------------------"
 echo
 
-${BACKEND_EXAMPLE_EXECUTABLE} monitor --time=1000 &
-# ${BACKEND_EXAMPLE_EXECUTABLE} monitor --time=1000 > /dev/null 2>&1 &
-BACKEND_EXAMPLE_PID=$!
+RESET_TIME=$((${ENTITIES_LOOP_ELAPSED}+((${SLEEP_RESIDUAL_TIME}*2)*${ENTITIES_IN_LOOP})+(${SLEEP_RESIDUAL_TIME}*2)))
+
+if [ $DEBUG -eq 1 ];
+then
+    ${BACKEND_EXAMPLE_EXECUTABLE} monitor ${RESET_BACKEND} ${BUMP_BACKEND} --time=${RESET_TIME} &
+    BACKEND_EXAMPLE_PID=$!
+else
+    ${BACKEND_EXAMPLE_EXECUTABLE} monitor ${RESET_BACKEND} ${BUMP_BACKEND} --time=${RESET_TIME} > /dev/null 2>&1 &
+    BACKEND_EXAMPLE_PID=$!
+fi
 
 
 ############################################################
@@ -256,16 +276,21 @@ do
     echo
 
     # Excute as many entities as required
+    declare -a fastdds_entities_pid=()
     # TODO
-    FASTDDS_STATISTICS="${STATISTIC_TOPICS}" ${FASTDDS_EXAMPLE_EXECUTABLE} publisher --interval ${PUBLICATION_RATE} --topic "TestTopic${i}" > /dev/null 2>&1 &
-    PUBLISHER_PID=$!
-    sleep ${SLEEP_RESIDUAL_TIME}
-    FASTDDS_STATISTICS="${STATISTIC_TOPICS}" ${FASTDDS_EXAMPLE_EXECUTABLE} subscriber --topic "TestTopic${i}" > /dev/null 2>&1 &
-    SUBSCRIBER_PID=$!
-    sleep ${SLEEP_RESIDUAL_TIME}
+    for j in $(seq 1 ${ENTITIES_IN_LOOP});
+    do
+        FASTDDS_STATISTICS="${STATISTIC_TOPICS}" ${FASTDDS_EXAMPLE_EXECUTABLE} publisher --interval ${PUBLICATION_RATE} --topic "TestTopic${i}" > /dev/null 2>&1 &
+        fastdds_entities_pid+=($!)
+        sleep ${SLEEP_RESIDUAL_TIME}
 
-    # Store memory usage measure
-    print_memory_usage "${BACKEND_EXAMPLE_PID}" "${LIBRARIES_TO_MEASURE}" "${RESULT_FILE_PATH}"
+        FASTDDS_STATISTICS="${STATISTIC_TOPICS}" ${FASTDDS_EXAMPLE_EXECUTABLE} subscriber --topic "TestTopic${i}" > /dev/null 2>&1 &
+        fastdds_entities_pid+=($!)
+        sleep ${SLEEP_RESIDUAL_TIME}
+
+        # Store memory usage measure
+        print_memory_usage "${BACKEND_EXAMPLE_PID}" "${LIBRARIES_TO_MEASURE}" "${RESULT_FILE_PATH}"
+    done
 
     # Sleep for some time
     sleep ${ENTITIES_LOOP_ELAPSED}
@@ -275,10 +300,17 @@ do
 
     # Kill entities
     echo "Killing Fast DDS Entities"
-    kill ${PUBLISHER_PID}
-    kill ${SUBSCRIBER_PID}
+    for pid_value in "${fastdds_entities_pid[@]}"
+    do
+        echo "Killing PID ${pid_value}"
+        kill $pid_value
+    done
 
     # Store memory usage measure
+    sleep ${SLEEP_RESIDUAL_TIME}
+    print_memory_usage "${BACKEND_EXAMPLE_PID}" "${LIBRARIES_TO_MEASURE}" "${RESULT_FILE_PATH}"
+
+    # Wait for data to be removed and store measure
     sleep ${SLEEP_RESIDUAL_TIME}
     print_memory_usage "${BACKEND_EXAMPLE_PID}" "${LIBRARIES_TO_MEASURE}" "${RESULT_FILE_PATH}"
 
